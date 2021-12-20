c44a806c9fa6efc62c8adbf3d850e97356b73f8b
pchink <cupuchino@gmail.com>
2021-12-20 20:06:15 +0300

    edit pandas exercise and ex2

---
+++ pandas/exercise.py
@@ -0,0 +1,34 @@
+import pandas as pd
+countries = pd.Series(
+    data = ['Англия', 'Канада', 'США', 'Россия', 'Украина', 'Беларусь', 'Казахстан'],
+    index = ['UK', 'CA', 'US', 'RU', 'UA', 'BY', 'KZ'],
+    name = 'countries'
+)
+#display(countries)
+print(countries)
+
+#  method 2
+countries = pd.Series({
+    'UK': 'Англия',
+    'CA': 'Канада',
+    'US' : 'США',
+    'RU': 'Россия',
+    'UA': 'Украина',
+    'BY': 'Беларусь',
+    'KZ': 'Казахстан'},
+    name = 'countries'
+)
+#display(countries)
+print(countries)
+
+#доступ к данным в series
+
+print(countries.loc['US'])
+
+#доступ к нескольким данным, оборачиваем в скобки
+
+print(countries.loc[['US', 'RU', 'UK']])
+
+# принимаем на вход порядковый номер элемента Series
+
+print(countries.iloc[6])
